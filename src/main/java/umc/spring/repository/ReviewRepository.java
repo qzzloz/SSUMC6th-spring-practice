c0a5eff59f58e5dc42a007c176d6b1de7d32bb20
qzzloz <xlivsxd@gmail.com>
2024-06-26 21:38:52 +0900

    feat: 내가 작성한 리뷰 목록 조회 API 구현

--- src/main/java/umc/spring/repository/ReviewRepository.java
+++ src/main/java/umc/spring/repository/ReviewRepository.java
@@ -4,10 +4,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import umc.spring.domain.Member;
 import umc.spring.domain.Mission;
 import umc.spring.domain.Review;
 import umc.spring.domain.Store;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findAllReviewByStore(Store store, PageRequest pageRequest);
+    Page<Review> findAllReviewByMember(Member member, PageRequest pageRequest);
 }
